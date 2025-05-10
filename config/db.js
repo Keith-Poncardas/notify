168ba1bb23a21921b427fc16ddb0fe766e1ed303
Keith Poncardas <poncardask03@gmail.com>
2025-05-10 14:30:46 +0800

    Changing DB config

--- config/db.js
+++ config/db.js
@@ -10,7 +10,10 @@ const logger = require('../utils/logger');
  */
 const connectDB = async () => {
     try {
-        const conn = await connect(process.env.MONGO_URL);
+        const conn = await connect(process.env.MONGO_URL, {
+            serverSelectionTimeoutMS: 30000,
+            socketTimeoutMS: 30000
+        });
         logger.success(`DB Connected to: ${conn.connection.host}`);
     } catch (err) {
         logger.error(err.message);
