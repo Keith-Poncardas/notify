d528aa7cdd89cb29403ee93e0fb948877902569e
Keith Poncardas <poncardask03@gmail.com>
2025-05-10 14:42:13 +0800

    Fixing vercel error (2)

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ app.use('/auth', authRoutes);
 app.use(pageNotFound);
 app.use(errorHandler);
 
-await connectDB();
+connectDB();
 app.listen(PORT, () => logger.success(`Server is connected to http://localhost:${PORT}`));
