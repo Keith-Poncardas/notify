90ecd12a1d30583d3d173a548766ab16a9f810fb
Keith Poncardas <poncardask03@gmail.com>
2025-05-24 14:16:26 +0800

    Fixing jquery

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ app.use(authenticateUser);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(express.json());
